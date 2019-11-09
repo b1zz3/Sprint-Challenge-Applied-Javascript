53c102bb9f314349130afb1bbe7f2423c12650a4
b1zz3 <donhager23@gmail.com>
2019-11-09 15:19:24 -0500

    Add api call for articles, build and add articles to DOM

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,56 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// api call
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        console.log('we workin', response);
+        const data = response.data.articles;
+        console.log('artys', data);
+        
+        
+        var key;
+        for (key in data) {
+            console.log('key test', key);
+            data[key].forEach(item => {
+                console.log('item test', item);
+            
+                const newCard = document.createElement('div');
+                const headln = document.createElement('div');
+                const authDiv = document.createElement('div');
+                const imageDiv = document.createElement('div');
+                const image = document.createElement('img');
+                const auth = document.createElement('span');
+
+                // assign content
+                headln.textContent = item.headline;
+                image.src = item.authorPhoto;
+                auth.textContent = item.authorName;
+
+                // assign classes
+                newCard.classList.add('card');
+                headln.classList.add('headline');
+                authDiv.classList.add('author');
+                imageDiv.classList.add('img-container');
+
+                // parent child relationship
+                newCard.appendChild(headln);
+                newCard.appendChild(authDiv);
+
+                authDiv.appendChild(imageDiv);
+                authDiv.appendChild(auth);
+
+                imageDiv.appendChild(image);
+                
+                // add card to DOM
+                const cardEntry = document.querySelector('.cards-container');
+                cardEntry.appendChild(newCard);
+
+            })
+        }
+    })
+    .catch(error => {
+        console.log('mo code mo problems', error);
+    })
